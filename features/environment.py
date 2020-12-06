a1944f18f0dfa191dfd4c13bcf2ea86b95af5afb
vassilisanagnostopoulos <vasilis.anagnostopoulos@agileactors.com>
2020-12-06 20:16:26 +0200

    Make more beautiful

--- features/environment.py
+++ features/environment.py
@@ -5,14 +5,8 @@ import time
 import threading
 import os
 import tempfile
-        
 
-def thread_function():
-    global srv
-    srv.serve_forever()
-    
-     
-def before_feature(context, feature):
+def create_server():
     global db_fname
     
     _, db_fname = tempfile.mkstemp()
@@ -29,7 +23,15 @@ def before_feature(context, feature):
     with app.app_context() as app_context:
         flaskql.db.init_db()
         app_context.push()
+            
+
+def thread_function():
+    global srv
+    srv.serve_forever()
     
+     
+def before_feature(context, feature):
+    create_server()
     context.testing_thread = threading.Thread(target=thread_function)
     context.testing_thread.start()
     time.sleep(2)
@@ -37,6 +39,7 @@ def before_feature(context, feature):
     
 
 def after_feature(context, feature):
+    global srv
     srv.shutdown()
     context.testing_thread.join()
     
