6e682c93e167416cecbb46e368c4bf5306d16373
vassilisanagnostopoulos <vasilis.anagnostopoulos@agileactors.com>
2020-12-07 09:20:37 +0200

    Refactorings

--- features/environment.py
+++ features/environment.py
@@ -1,47 +1,40 @@
 from werkzeug.serving import make_server
 import flaskql 
-import time   
 
 import threading
 import os
 import tempfile
 
-def create_server():
-    global db_fname
-    
-    _, db_fname = tempfile.mkstemp()
-    app = flaskql.create_app({
-        'DATABASE': db_fname,
-            'TESTING': True,
-            'ENV' : 'development'
+class MyServer :
+    def  __init__(self):
+        _, db_fname = tempfile.mkstemp()
+        app = flaskql.create_app({
+            'DATABASE': db_fname,
+                'TESTING': True,
+                'ENV' : 'development'})
+        app.debug = False
+        self.db_fname = db_fname
+        self.srv = make_server('127.0.0.1', 5000, app)
+ 
+        with app.app_context() as app_context:
+            flaskql.db.init_db()
+            app_context.push()
             
-            })
-    app.debug = False
-    global srv
-    srv = make_server('127.0.0.1', 5000, app)
-
-    with app.app_context() as app_context:
-        flaskql.db.init_db()
-        app_context.push()
+    def start(self):
+        self.srv.serve_forever() 
+           
+    def shutdown(self):
+        self.srv.shutdown()
+        os.unlink(self.db_fname)
             
-
-def thread_function():
-    global srv
-    srv.serve_forever()
+def thread_function(context):
+    context.server.start()
     
-     
 def before_feature(context, feature):
-    create_server()
-    context.testing_thread = threading.Thread(target=thread_function)
+    context.server = MyServer()
+    context.testing_thread = threading.Thread(target=thread_function, args=(context,))
     context.testing_thread.start()
-    time.sleep(2)
-    
-    
 
 def after_feature(context, feature):
-    global srv
-    srv.shutdown()
+    context.server.shutdown()
     context.testing_thread.join()
-    
-    global db_fname
-    os.unlink(db_fname)
